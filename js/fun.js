ae757addc93e363fde9bd2bd9d83858d1470990a
Raja Kedia <rajakedia2222@gmail.com>
2017-03-06 23:36:55 +0530

    Added js

---
+++ js/fun.js
@@ -0,0 +1,56 @@
+$('.left').css({
+            
+    'height' : $(window).height() 
+});
+$('.right .skill').css({
+            
+    'height' : $(window).height() 
+});
+$('.right .main').css({
+            
+    'height' : $(window).height(),
+    
+});
+$('.right .devl').css({
+            
+    'height' : $(window).height(),
+    
+});
+$('.right .proj').css({
+            
+    'height' : $(window).height(),
+    
+});
+$('.right .main h1').css({
+            
+    'padding-top' : ($(window).height()/2) - 130
+    
+});
+
+$('.right .proj .pro h1').css({
+            
+    'padding-top' : ($(window).height()/2) - 250
+    
+});
+
+$('.right .skillset').css({
+            
+    'padding-top' : ($(window).height()/2) - 260
+    
+});
+
+$(window).scroll(function(){
+    var wScroll = $(this).scrollTop();
+if(wScroll > $('.skill .skillset').offset().top - ($(window).height()/1.2)){  // skill set
+        
+        $('.skill .skillset li').each(function(i){
+            
+            setTimeout(function(){
+                
+                $('.skill .skillset li').eq(i).addClass('is-showing2');
+                
+            },150 * (i+1));
+        });
+        
+    }
+});
---
+++ js/slider.js
@@ -0,0 +1,10 @@
+$('#slider').cycle({
+    
+    fx:   'scrollHorz',
+    next: '#next',
+    prev: '#prev',
+    pager: '#pager',
+    timeout: 1000,
+    speed:  700
+    
+});
